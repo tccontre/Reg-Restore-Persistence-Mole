4f385bd9951766e13f6c7ec695ea18cc2ce876b2
tccontre <26181693+tccontre@users.noreply.github.com>
2023-08-18 17:23:00 +0200

    Create stdafx.h

---
+++ RegReeper/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <winreg.h>
+#include <time.h>
+#include <tchar.h>
+#include <string.h>
+#include <strsafe.h>
+#include <fcntl.h>
+#include <io.h>
+#include <stdio.h>
